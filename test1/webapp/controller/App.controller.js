e036bf41f2c6717467e2962cd5e27fbfc7eb672c
santosh gundelli <santoshr.g@hotmail.com>
2024-03-06 20:10:47 +0000

    Initial Push

---
+++ test1/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("test1.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
